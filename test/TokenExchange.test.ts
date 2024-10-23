da50f5c1038b6ec2e139ca43232dbb2bb932b4c0
AntonVoronezh <pstut@mail.ru>
2024-10-23 17:32:25 +0300

    2

--- test/TokenExchange.test.ts
+++ test/TokenExchange.test.ts
@@ -16,26 +16,40 @@ describe("TokenExchange", function () {
     return { abcdf, exch, owner, buyer };
   }
 
-  // it("should allow to add  albums", async function () {
-  //   const { shop } = await loadFixture(deploy);
-  //
-  //   const title = "Demo";
-  //   const price = 100;
-  //   const uid = ethers.solidityPackedKeccak256(["string"], [title]);
-  //   const qty = 5;
-  //   const initialIndex = 0;
-  //
-  //   const addTx = await shop.addAlbum(uid, title, price, qty);
-  //   await addTx.wait();
-  //
-  //   const album  = await shop.albums(initialIndex);
-  //
-  //   expect(album.index).to.eq(initialIndex);
-  //   expect(album.uid).to.eq(uid);
-  //   expect(album.price).to.eq(price);
-  //   expect(album.quantity).to.eq(qty);
-  //
-  //   expect(await shop.currentIndex()).to.eq(initialIndex + 1);
-  // });
+  it("should allow to buy", async function () {
+    const { abcdf, exch, owner, buyer } = await loadFixture(deploy);
+
+    const tokensInStock = 3n;
+    const tokensWithDecimals = await withDecimals(abcdf, tokensInStock);
+
+    const transferTx = await abcdf.transfer(exch.target, tokensWithDecimals);
+
+    // expect(abcdf.balanceOf(exch.target)).to.eq(tokensWithDecimals);
+    await expect(transferTx).to.changeTokenBalances(
+        abcdf, [owner, exch], [-tokensWithDecimals, tokensWithDecimals]
+    );
+
+    const tokensToBuy = 1n;
+    const value  = ethers.parseEther(tokensToBuy.toString());
+
+    const buyTx = await  exch.connect(buyer).buy({value: value});
+    await buyTx.wait();
+
+    await expect(buyTx).to.changeEtherBalances([buyer, exch], [-value, value]);
+    await expect(buyTx).to.changeTokenBalances(abcdf, [exch, buyer], [-value, value]);
+  });
+
+  it("should allow to sell", async function () {
+    const { abcdf, exch, owner, buyer } = await loadFixture(deploy);
+
+    const tokensInStock = 3n;
+    const tokensWithDecimals = await withDecimals(abcdf, tokensInStock);
+
+
+  });
+
+  async function withDecimals(token: ABCDFToken, value: bigint): Promise<bigint> {
+    return value * 10n ** await token.decimals();
+  }
 
 });
